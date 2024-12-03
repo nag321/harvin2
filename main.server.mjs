7dae682ba7c2742f041802fae24964abb7f32da3
nag321 <134135792+nag321@users.noreply.github.com>
2024-12-03 19:42:46 +0530

    new v1 landing page

--- main.server.mjs
+++ main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SGCA7XQZ.mjs";import"./chunk-TXW3XUKZ.mjs";import"./chunk-TMAF5LTA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-FMI3IB2N.mjs";import"./chunk-TXW3XUKZ.mjs";import"./chunk-TMAF5LTA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
